d28305548f7bf18de6f32f8de73b563c940b07fb
Viktor <vjagebrant@gmail.com>
2022-10-12 19:25:17 +0200

    fyller telefonboken

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         Map<String, String> phoneBook = new HashMap<>();
+
+        phoneBook.put("John", "0729385745");
+        phoneBook.put("Doug", "0783957283");
+        phoneBook.put("Clair", "0784392345");
+        phoneBook.put("Jenny", "0792484883");
+        phoneBook.put("Bob", "0784739874");
     }
 }
