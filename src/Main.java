c6609e5f931c9c684ee2f117f16188615cdefdf1
Viktor <vjagebrant@gmail.com>
2022-10-12 19:53:24 +0200

    made the loop

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Map<String, String> phoneBook = new HashMap<>();
         Scanner sc = new Scanner(System.in);
+        while(true) {
+            phoneBook.put("John", "0729385745");
+            phoneBook.put("Doug", "0783957283");
+            phoneBook.put("Clair", "0784392345");
+            phoneBook.put("Jenny", "0792484883");
+            phoneBook.put("Bob", "0784739874");
 
-        phoneBook.put("John", "0729385745");
-        phoneBook.put("Doug", "0783957283");
-        phoneBook.put("Clair", "0784392345");
-        phoneBook.put("Jenny", "0792484883");
-        phoneBook.put("Bob", "0784739874");
-
-        System.out.println("Enter the name of the person to look up: ");
-        String choice = sc.nextLine();
-        if (phoneBook.containsKey(choice)) {
-            System.out.println(choice + " has the phone number: " + phoneBook.get(choice));
-        } else{
-            System.out.println(choice + " isn't in the phone book.");
+            System.out.println("Enter the name of the person to look up: ");
+            String choice = sc.nextLine();
+            if (phoneBook.containsKey(choice)) {
+                System.out.println(choice + " has the phone number: " + phoneBook.get(choice));
+            } else {
+                System.out.println(choice + " isn't in the phone book.");
+            }
         }
     }
 }
